4db84673faa1488bebc4607cc9dcbd2ac78fa1ce
Daniel Lessenden <dless27@gmail.com>
2020-08-05 10:59:05 -0500

    Add total_inventory method for event class

--- lib/event.rb
+++ lib/event.rb
@@ -26,5 +26,18 @@ attr_reader :name, :food_trucks
     trucks_that_sell
   end
 
+  def total_inventory
+    event_inventory = Hash.new do |event_inventory, item|
+      event_inventory[item] = {quantity: 0, food_trucks: []}
+    end
+    @food_trucks.each do |truck|
+      truck.inventory.each do |truck_item, quantity|
+        event_inventory[truck_item][:quantity] += quantity
+        event_inventory[truck_item][:food_trucks] << truck
+      end
+    end
+    event_inventory
+  end
+
 
 end
